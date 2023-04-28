90a56b802eb0a806f089ebaa1baaedb35e0b6ebd
Joshua Alwinking <65503372+JoshuaisaKing@users.noreply.github.com>
2023-04-28 21:04:27 +0530

    Add files via upload

---
+++ Use_Case_1(Library)/src/app/model/books.ts
@@ -0,0 +1,9 @@
+export class books{
+    id:number = 0;
+    borrowed_by:string = "";
+    book_name:string = "";
+    borrow_date:string = "";
+    return_by:string = "";
+    author:string = "";
+    returned:string = "";
+}
