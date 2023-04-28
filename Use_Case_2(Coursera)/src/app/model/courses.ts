d6046a74af1f2d73930b3c7c82a13ffb229ffef3
Joshua Alwinking <65503372+JoshuaisaKing@users.noreply.github.com>
2023-04-28 21:12:44 +0530

    Add files via upload

---
+++ Use_Case_2(Coursera)/src/app/model/courses.ts
@@ -0,0 +1,6 @@
+export class courses{
+    id:number = 0;
+    course_name:string = "";
+    author:string = "";
+    course_level:string="";
+}
---
+++ Use_Case_2(Coursera)/src/app/model/user_courses.ts
@@ -0,0 +1,6 @@
+export class usercourses{
+    id:number = 0;
+    course_name:string = "";
+    author:string = "";
+    course_level:string="";
+}
