3643465c3aa6d937e5366531c69ea3b90940f31f
Joshua Alwinking <65503372+JoshuaisaKing@users.noreply.github.com>
2023-04-28 21:11:07 +0530

    Add files via upload

---
+++ Use_Case_2(Coursera)/src/app/admin_courses/admin_courses.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CourseComponent } from './admin_courses.component';
+
+describe('BooksComponent', () => {
+  let component: CourseComponent;
+  let fixture: ComponentFixture<CourseComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CourseComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CourseComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
