b73b856cd9044463b97c5003b79d4d7670b946d1
Joshua Alwinking <65503372+JoshuaisaKing@users.noreply.github.com>
2023-05-31 17:44:39 +0530

    Delete Use_Case_2(Coursera) directory

--- Use_Case_2(Coursera)/src/app/model/courses.ts
+++
@@ -1,6 +0,0 @@
-export class courses{
-    id:number = 0;
-    course_name:string = "";
-    author:string = "";
-    course_level:string="";
-}
--- Use_Case_2(Coursera)/src/app/model/user_courses.ts
+++
@@ -1,6 +0,0 @@
-export class usercourses{
-    id:number = 0;
-    course_name:string = "";
-    author:string = "";
-    course_level:string="";
-}
