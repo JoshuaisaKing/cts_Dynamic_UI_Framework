88cd84ef074f631d8919f5d44c82f024467a8c41
Joshua Alwinking <65503372+JoshuaisaKing@users.noreply.github.com>
2023-06-13 09:36:28 +0530

    Delete Use_Case_1(Library) directory

--- Use_Case_1(Library)/src/app/model/books.ts
+++
@@ -1,9 +0,0 @@
-export class books{
-    id:number = 0;
-    borrowed_by:string = "";
-    book_name:string = "";
-    borrow_date:string = "";
-    return_by:string = "";
-    author:string = "";
-    returned:string = "";
-}
--- Use_Case_1(Library)/src/app/model/signup.ts
+++
@@ -1,6 +0,0 @@
-export class ISignup{
-    [x: string]: any;
-    name:string='';
-    email:string='';
-    password:string='';
-}
