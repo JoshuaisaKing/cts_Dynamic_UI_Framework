12c96a00f609340f3da7461a9d5ce968c5535e41
Joshua Alwinking <65503372+JoshuaisaKing@users.noreply.github.com>
2023-06-13 19:28:57 +0530

    Add files via upload

---
+++ projects/advanced-search/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of advanced-search
+ */
+
+export * from './lib/advanced-search.service';
+export * from './lib/advanced-search.component';
+export * from './lib/advanced-search.module';
