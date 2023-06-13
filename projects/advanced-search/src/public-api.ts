e6e222cf95b89735a9270dde3a5252275ebdce97
Joshua Alwinking <65503372+JoshuaisaKing@users.noreply.github.com>
2023-06-13 19:26:59 +0530

    Delete projects/advanced-search directory

--- projects/advanced-search/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of advanced-search
- */
-
-export * from './lib/advanced-search.component';
-export * from './lib/advanced-search.module';
-export * from './lib/models';
-export * from './lib/filter-save-popup/filter-save-popup.component';
